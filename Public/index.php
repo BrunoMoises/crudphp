62d99bcfb56d08d82dd9ce95df8c0b7029b1e888
Bruno <bruno_moises_stz@hotmail.com>
2023-01-28 15:42:42 -0300

    Finalizando telas, layouts e README do projeto

--- Public/index.php
+++ Public/index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.js"></script>
 </head>
 
@@ -16,86 +17,38 @@
     <div class="container">
         <header>
             <div class="row mt-3">
-                <div class="col-md-6">
-                    <img src="./img/logo.png" alt="CRUD" style="max-width: 40px;">
+                <div class="col-md text-center">
+                    <a href="index.php"><img class="pointer" src="./img/logo.png" alt="CRUD"
+                            style="max-width: 60px;"></a>
                 </div>
-                <div class="col-md-6 text-end">
-                    <button type="button" class="btn btn-outline-info" id="bt-new">Nova pessoa</button>
-                </div>
+            </div>
         </header>
         <hr class="border-info">
         <div class="row mt-3">
-            <h1>Tabela de Pessoas: </h1>
-            <table class="table table-striped-columns table-bordered">
-                <thead>
-                    <tr>
-                        <th colspan="3">Codigo</th>
-                        <th>Nome</th>
-                        <th>E-mail</th>
-                        <th>Categoria</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                </tbody>
-            </table>
-            <script id="tmplLinha" type="text/template">
-                <tr>
-                    <td width="10"><i class="bi-x-circle-fill icon icon-x pointer" title="Excluir pessoa"
-                            aria-multiline="Excluir pessoa" onclick="deletePessoa('{{id_pessoa}}')"></i></td>
-                    <td width="10"><i class="bi-pencil-fill icon icon-edit pointer" title="Editar pessoa"
-                            aria-multiline="Editar pessoa" onclick="editaPessoa('{{id_pessoa}}')"></i></td>
-                    <td align="center" width="40">{{id_pessoa}}</td>
-                    <td>{{Nome}}</td>
-                    <td>{{Email}}</td>
-                    <td>{{id_categoria}}</td>
-                </tr>
-            </script>
+            <div class="col-md text-center">
+                <h1>Olá, seja bem vindo ao meu projeto de CRUD PHP com APIRest!</h1>
+                <p>
+                    Este foi um projeto criado sozinho do zero onde o intuito era criar um CRUD em PHP com MySQL
+                    consumindo os dados através de uma APIRest.
+                </p>
+                <p>
+                    Para desenvolvimento foi utilizado para simular o ambiente local o XAMPP, com PHP versão 8.2.0 e
+                    MySQL versão 10.4.27.
+                </p>
+                <p>
+                    Para iniciar o projeto basta clicar no botão abaixo:
+                </p>
+                <p>
+                    <a href="inicio.php"><button type="button" class="btn btn-lg btn-outline-info"
+                            id="bt-new">Iniciar</button></a>
+                </p>
+                <p>
+                    Para acessar o projeto acesse&nbsp;&nbsp;<a href="https://github.com/BrunoMoises/crudphp"
+                        target="_blank"><i class="bi-github icon icon-git pointer"></i></a>
+                </p>
+            </div>
         </div>
     </div>
-
-    <div class="modal fade" id="modalNovaPessoa" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <form id="frmCreate" method="post">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Criar/Editar Pessoa</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <label for="txtNome">Nome</label>
-                                <input type="hidden" id="txtId" value="">
-                                <input type="text" id="txtNome" placeholder="Preencha o nome..." class="form-control">
-                            </div>
-                            <div class="col-md-4">
-                                <label for="txtEmail">E-mail</label>
-                                <input type="email" id="txtEmail" placeholder="Preencha o E-mail..."
-                                    class="form-control">
-                            </div>
-                            <div class="col-md-4">
-                                <label for="seCategoria">Categoria</label>
-                                <select id="seCategoria" class="form-control">
-                                    <option value="3">Normal</option>
-                                    <option value="2">Gerente</option>
-                                    <option value="1">Admin</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Fechar</button>
-                        <button type="submit" class="btn btn-success" id="btnSubmit">Salvar</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-
-    <script src="js/jquery-3.6.3.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/script.js"></script>
 </body>
 
 </html>
